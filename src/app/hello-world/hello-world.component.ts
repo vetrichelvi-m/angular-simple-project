c0df9f7f7dc46ccb037c8d6256b38d2506d061fd
vetri <vetrichelvi26@gmail.com>
2024-02-02 22:08:51 +0530

    change

---
+++ src/app/hello-world/hello-world.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hello-world',
+  templateUrl: './hello-world.component.html',
+  styleUrl: './hello-world.component.css'
+})
+export class HelloWorldComponent {
+  firstname =  'vetri';
+  secondname = 'selvi';
+
+}
